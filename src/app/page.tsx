e3e3fbad370ec5f1eaf284cda89b2661503394e1
jamesdiffeycoding <jamesadiffey@gmail.com>
2025-01-29 14:58:28 +0000

    add responsive UI with rendered names, prices, images and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { TMenuSection, TMenu } from "../../types";
+
 const url =
   "https://menus.flipdish.co/prod/16798/e6220da2-c34a-4ea2-bb51-a3e190fc5f08.json";
 
@@ -15,9 +16,45 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="">
-      <p>{sections.length ? "Yay" : "Loading"}</p>
-      {sections.length}
+    <div className="m-4 flex justify-center">
+      <div className="w-[90%] max-w-[1000px]">
+        <div className="flex flex-col gap-8 mt-4 justify-center">
+          {!sections.length && <p> Loading... </p>}
+
+          {sections?.map((category) => (
+            <div key={category.MenuSectionId}>
+              <h2
+                key={category.MenuSectionId}
+                className="text-xl pb-2 font-bold"
+              >
+                {category.Name}
+              </h2>
+              <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                {category?.MenuItems.map((item) => (
+                  <div
+                    key={item.MenuItemId}
+                    className="flex justify-between border p-2 border-slate-100 rounded-lg hover:border hover:bg-slate-100 cursor-pointer"
+                  >
+                    <div className="flex flex-col justify-between">
+                      <p className="font-bold">{item.Name}</p>
+                      <p className="text-sm">{item.Description}</p>
+                      <p className="text-sm">£{item.Price}</p>
+                    </div>
+                    <img
+                      src={item.ImageUrl || "/placeholder.jpg"}
+                      alt={`Image of ${item.Name}`}
+                      className="h-[100px] min-w-[100px] max-w-[50px] object-cover rounded-lg"
+                    />
+                  </div>
+                ))}
+              </div>
+              {category.MenuItems.length === 0 && (
+                <p>We're whipping something up - come back soon!</p>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="text-center bg-black text-white p-2 text-2xl">
+      Flipdish
+    </header>
+  );
+}
