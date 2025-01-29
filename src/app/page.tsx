f4196f4767eb1d4a384a426c95f52d320b711cdc
jamesdiffeycoding <jamesadiffey@gmail.com>
2025-01-29 13:02:06 +0000

    basic fetch and loading message  with axios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const url =
+  "https://menus.flipdish.co/prod/16798/e6220da2-c34a-4ea2-bb51-a3e190fc5f08.json";
+
 export default function Home() {
-  return <div className=""></div>;
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get(url).then(({ data }) => {
+      setItems(data);
+    });
+  }, []);
+
+  return (
+    <div className="">
+      <p>{items.MenuId ? "Yay" : "Loading"}</p>
+    </div>
+  );
 }
