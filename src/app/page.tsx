70c258fd7af6a8a678cdb0d7e45be49fce1b5e69
jamesdiffeycoding <jamesadiffey@gmail.com>
2025-01-29 13:35:39 +0000

    add type checking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,23 @@
 "use client";
 import axios from "axios";
 import { useState, useEffect } from "react";
-
+import { TMenuSection, TMenu } from "../../types";
 const url =
   "https://menus.flipdish.co/prod/16798/e6220da2-c34a-4ea2-bb51-a3e190fc5f08.json";
 
 export default function Home() {
-  const [items, setItems] = useState([]);
+  const [sections, setSections] = useState<TMenuSection>([]); // empty array allowed as values are optional
 
   useEffect(() => {
-    axios.get(url).then(({ data }) => {
-      setItems(data);
+    axios.get<TMenu>(url).then(({ data }) => {
+      setSections(data.MenuSections);
     });
   }, []);
 
   return (
     <div className="">
-      <p>{items.MenuId ? "Yay" : "Loading"}</p>
+      <p>{sections.length ? "Yay" : "Loading"}</p>
+      {sections.length}
     </div>
   );
 }
